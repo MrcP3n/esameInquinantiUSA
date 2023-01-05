5bfb3e03b1c8ee3c83ba1091aa36e8fce2eeb687
MrcP3n <marco.pennicchi@gmail.com>
2023-01-05 16:59:33 +0100

    Da fare

---
+++ Scriptpython/studio5Stati.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+from scipy import stats
+import matplotlib.pyplot as plt
+from scipy import constants, fft
+import scipy.optimize as opt
+
+
+
+tab=pd.read_csv('')
+
+
+
