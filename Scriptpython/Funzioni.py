04b27d19f929dc18cbf634bf466ab1958cd014d4
MrcP3n <marco.pennicchi@gmail.com>
2023-02-08 14:30:34 +0100

    aaaaaaa

--- Scriptpython/Funzioni.py
+++ Scriptpython/Funzioni.py
@@ -1,5 +1,6 @@
 from scipy import constants,fft
 import numpy as np
+import statistics
 import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.ticker import (MultipleLocator,AutoMinorLocator)
@@ -102,16 +103,37 @@ def meanday(stato):
     aMeanday=np.empty(0)
     aDate=np.empty(0)
     date=np.empty(0)
+    rip=np.array([0])
+    j=0
+    z=1
+    arr=np.empty(0)
+    aMediaDay=np.empty(0)
     for h in stato.arrRil:
         aMeanday = np.append(aMeanday ,h.mean)
         aDate = np.append(aDate ,h.data)
     for i in range(len(aDate)-1):
         if(aDate[i]!=aDate[i+1]):
             date=np.append(date,aDate[i])
+            rip=np.append(rip,i-j)
+            j=i
         if (i==(len(aDate)-2)):
             date=np.append(date,aDate[i])
 
-    return aMeanday , date
+    for i in range(len(aMeanday)):
+        arr=np.append(arr,aMeanday[i])
+        if(i==rip[z]):
+            m=statistics.mean(arr)
+            aMediaDay=np.append(aMediaDay,m)
+            arr=np.empty(0)
+            z=z+1
+        if(i==rip[z] and aMediaDay.size >= 1):
+            m=statistics.mean(arr)
+            aMediaDay=np.append(aMediaDay,m)
+            b = arr
+            arr=np.empty(0)
+            z=z+1
+
+    return aMediaDay , aDate ,arr
 
 def graphInTime2(arrx1,arry1,arrx2,arry2,title,cl):
     fig, (ax1, ax2) = plt.subplots(2)
@@ -160,6 +182,7 @@ def graphInTime5(arrx1,arry1,arrx2,arry2,arrx3,arry3, arrx4,arry4,arrx5,arry5,ti
     fig.set_figwidth(10)
     plt.savefig(title)
     plt.show()
+    
     return
     
 
@@ -180,23 +203,16 @@ def graphSpettri5(acof1,afreq1,acof2,afreq2,acof3,afreq3,acof4,afreq4,acof5,afre
     ax1.plot(afreq1[1:int(acof1.size/2)],np.absolute(acof1[1:int(acof1.size/2)])**2,color='red')
     ax1.set_xscale('log')
     ax1.set_yscale('log')
-    #ax1.set_yscale('log')
     ax2.plot(afreq2[:int(acof2.size/2)],np.absolute(acof2[:int(acof2.size/2)])**2,color='limegreen')
     ax2.set_xscale('log')
     ax2.set_yscale('log')
-    #ax[0,1].set_xscale('log')
-    #ax[0,1].set_yscale('log')
     ax3.plot(afreq3[:int(acof3.size/2)],np.absolute(acof3[:int(acof3.size/2)])**2,color='cyan')
     ax3.set_xscale('log')
     ax3.set_yscale('log')
-    #ax[0,2].set_xscale('log')
-    #ax[0,2].set_yscale('log')
     ax3.set_ylabel('$|c_{FFT}|^2$ [$\mu g^2/m^6$]') #Migliora
     ax4.plot(afreq4[:int(acof4.size/2)],np.absolute(acof4[:int(acof4.size/2)])**2,color='midnightblue')
     ax4.set_xscale('log')
     ax4.set_yscale('log')
-    #ax[1,0].set_xscale('log')
-    #ax[1,0].set_yscale('log')
     ax5.plot(afreq5[:int(acof5.size/2)],np.absolute(acof5[:int(acof5.size/2)])**2,color='orchid')
     ax5.set_xscale('log')
     ax5.set_yscale('log')
@@ -233,17 +249,4 @@ def graphSpettri5Per(acof1,afreq1,acof2,afreq2,acof3,afreq3,acof4,afreq4,acof5,a
 
     return
 
-'''
-def graphSpettri5(acof1,afreq1,acof2,afreq2,acof3,afreq3,acof4,afreq4,acof5,afreq5,title):
-    plt.title(title)
-    plt.plot(afreq1[:int(acof1.size/2)],np.absolute(acof1[:int(acof1.size/2)])**2,color='red')
-    plt.plot(afreq2[:int(acof2.size/2)],np.absolute(acof2[:int(acof2.size/2)])**2,color='limegreen')
-    plt.plot(afreq3[:int(acof3.size/2)],np.absolute(acof3[:int(acof3.size/2)])**2,color='cyan')
-    plt.plot(afreq4[:int(acof4.size/2)],np.absolute(acof4[:int(acof4.size/2)])**2,color='midnightblue')
-    plt.plot(afreq5[:int(acof5.size/2)],np.absolute(acof5[:int(acof5.size/2)])**2,color='orchid')
-    plt.xlabel('Frequenze')
-    plt.ylabel('Coefficenti')
-    plt.show()
 
-    return
-'''
--- Scriptpython/studioStati.py
+++ Scriptpython/studioStati.py
@@ -4,6 +4,7 @@ import Classi
 import matplotlib.pyplot as plt
 from scipy import constants , fft
 import Funzioni as f
+import sys
 
 pstati='/home/marco-ubu/esameInquinantiUSA/DatiProgetto/datiStati.csv'
 
@@ -11,10 +12,10 @@ rilStati=f.getRilevaz(pstati)
 
 rilStati.sort(kind='mergesort')
 #print(ril5Stati)
-
+'''
 for h in rilStati:
     print(h.cStato, h.cContea, h.cStaz, h.data, h.mean)
-
+'''
 print('Numero totale di rilevazioni:', rilStati.size)
 '''
 stazStati=f.getStazioni(rilStati)
@@ -33,19 +34,25 @@ print('Numero totale di stazioni: ', stazStati.size)
 stati,acod=f.getStati(rilStati)
 stati.sort(kind='mergesort')
 
-print('------------------------')
 
+'''
 for hh in stati[0].arrRil:
     print(hh.cStato,hh.data,hh.mean)
-
+'''
 
 for i in range(34):
     print('-----Stato{:}------'.format(i))
     print('nRil', stati[i].nril)
 print('Numero totale di stati: ',stati.size, 'codici', acod)
-
-meanO3AZ , dateAZ =f.meanday(stati[0])
+print('------------------------')
+meanO3AZ , dateAZ ,arr =f.meanday(stati[0])
+np.set_printoptions(threshold=sys.maxsize)
+'''
 print(dateAZ,' Lunghezza ',len(dateAZ))
+'''
+print(meanO3AZ ,' Lunghezza ',len(meanO3AZ),len(dateAZ))
+print(arr)
+
 
 
 
